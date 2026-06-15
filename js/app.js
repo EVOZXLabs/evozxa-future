9ce5680b935efb16d5449fe06b1327a0ffb1f674
EVOZXLaunch <evozxtoken@gmail.com>
2026-06-15 21:56:04 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
-console.log(
-"EVOZXA FUTURE INITIALIZED"
+import { connectWallet } from "./wallet.js";
+
+const connectBtn =
+document.getElementById("connectBtn");
+
+connectBtn.addEventListener(
+    "click",
+    async () => {
+
+        try {
+
+            const wallet =
+            await connectWallet();
+
+            if (!wallet) return;
+
+            const shortAddress =
+                wallet.address.slice(0, 6) +
+                "..." +
+                wallet.address.slice(-4);
+
+            connectBtn.textContent =
+                shortAddress;
+
+        } catch (error) {
+
+            console.error(error);
+
+            alert(
+                error.message ||
+                "Wallet connection failed"
+            );
+
+        }
+
+    }
 );
